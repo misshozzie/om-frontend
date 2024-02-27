980e0154c78e3fcf7c6ca9b6909af67bcd821abc
Hosanna Martin <vivahosanna@icloud.com>
2024-02-27 14:35:33 +0800

    fixing error for deployment

--- officemate-frontend/src/Firebase/firebase.config.js
+++ officemate-frontend/src/Firebase/firebase.config.js
@@ -1,16 +1,15 @@
-    // Import the functions you need from the SDKs you need
-    // Import the functions you need from the SDKs you need
+
     import { initializeApp } from "firebase/app";
     import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
     const firebaseConfig = {
-      apiKey: "AIzaSyDxSzj3KeH5wvkpZXiEjsMAkr4KBL3yJOk",
-      authDomain: "officemate-4862e.firebaseapp.com",
-      projectId: "officemate-4862e",
-      storageBucket: "officemate-4862e.appspot.com",
-      messagingSenderId: "637219469946",
-      appId: "1:637219469946:web:b77ad0226c77e85fdcd9c3",
-      measurementId: "G-02JWTTH9VD"
+      apiKey: "VITE_apiKey",
+      authDomain: "VITE_authDomain",
+      projectId: "VITE_projectId",
+      storageBucket: "VITE_storageBucket",
+      messagingSenderId: "VITE_messagingSenderId",
+      appId: "VITE_appId",
+      measurementId: "VITE_MeasurementId"
     };
     
     export const app = initializeApp(firebaseConfig);
