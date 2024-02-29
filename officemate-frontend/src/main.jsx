af6c021dd449f92186dd769ef324d024bfbb5024
Hosanna Martin <vivahosanna@icloud.com>
2024-02-29 08:28:09 +0800

    Update main.jsx

--- officemate-frontend/src/main.jsx
+++ officemate-frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { AllProvider } from "./components/Pages/AllContext";
 import { router } from "./components/Paths/Router";
 import AuthProvider from "./components/Pages/Users/Authprovider";
 import "./index.css";
-import AuthProvider from "./components/Pages/Users/Authprovider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
